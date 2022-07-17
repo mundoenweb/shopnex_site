0720a2b499ce2a99bfc83d65d444dd7f588b8c01
rommer <rommer@tuemprende.com>
2022-07-17 00:43:47 -0500

    se crea handle para crear una noticia

---
+++ src/pages/createNews/utils/handleCreateNews.js
@@ -0,0 +1,27 @@
+import axios from "axios"
+import nProgress from "nprogress"
+import store from "../../../redux/store"
+
+export const handleCreateNews = (data, form, navigate) => {
+  nProgress.start()
+
+  const formData = new FormData(form)
+
+  const token = store.getState().user.token
+  const uri = process.env.REACT_APP_URL_API
+  const api = `${uri}/news`
+  const options = {
+    headers: { Authorization: `Bearer ${token}` }
+  }
+
+  axios.post(api, formData, options)
+    .then(res => {
+      console.log(res)
+      // navigate('/tareas')
+    })
+    .catch(err => {
+      console.log(err.response.data)
+      alert(err.response.data.messageError.message)
+    })
+    .then(() => nProgress.done())
+}
