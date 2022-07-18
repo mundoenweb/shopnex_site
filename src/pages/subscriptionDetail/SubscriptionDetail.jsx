ad7183b37183255748a35b2a6d6f17cb36e93ada
rommer <rommer@tuemprende.com>
2022-07-18 04:56:38 -0500

    se crea la pagina para ver el detalle de una suscripción

---
+++ src/pages/subscriptionDetail/SubscriptionDetail.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Main from '../../components/templetes/Main/Main'
+import useFetchGet from '../../hooks/useFetchGet'
+
+const SubscriptionDetail = () => {
+  const params = useParams()
+  const [subscription] = useFetchGet(`/subscriptions/${params.id}`)
+  // const uri = process.env.REACT_APP_URL_API
+
+
+  return (
+    <Main title='Detalle suscripción'>
+      {
+        !subscription.length &&
+        < div > Cargando detalles de la suscripción </div>
+      }
+      {
+        subscription.length > 0 &&
+        <div className='box_content content_options'>
+          <div>
+            <h2>{subscription[0].name}</h2>
+            <br />
+            <p>Costo: S/. {subscription[0].cost}</p>
+            <p>Comisión: {subscription[0].commission}%</p>
+            <p className='p_pre'>Descripción: {subscription[0].description}</p>
+          </div>
+        </div>
+      }
+    </Main >
+  )
+}
+
+export default SubscriptionDetail
