9bf5cf9caaeaeb57a0e9c0c3bf4370654cb2628b
rommer <rommer@tuemprende.com>
2022-07-24 14:41:21 -0500

    se modifica la lista de suscripciones para los clientes

--- src/pages/subscriptions/Subscriptions.jsx
+++ src/pages/subscriptions/Subscriptions.jsx
@@ -2,32 +2,44 @@ import ButtonsOptions from "../../components/common/ButtonsOptions"
 import useFetchGet from "../../hooks/useFetchGet"
 import { Link } from 'react-router-dom'
 import Main from "../../components/templetes/Main/Main"
+import { connect } from "react-redux"
 
-const Subscriptions = () => {
+const Subscriptions = ({
+  idSubsCription
+}) => {
 
   const [subscriptions] = useFetchGet('/subscriptions')
 
   return (
-    <Main title='Tipos de suscripciones'>
-        <div className="box_content">
-          {
-            subscriptions.map(subscription => (
-              <div key={subscription.id} className="item_list_general">
-                <div>
-                  <p className="title_item_list">{subscription.name}</p>
-                  <p className="title_descrip_list">Costo: S/ {subscription.cost}</p>
-                  <p className="title_descrip_list">Comisíón: {subscription.commission}%</p>
-                </div>
+    <Main title='Sube a premiun'>
+      <div className="box_content">
+        {
+          subscriptions.map(subscription => (
+            <div key={subscription.id} className="item_list_general">
+              <div>
+                <p className="title_item_list">{subscription.name} {
+                idSubsCription ===  subscription.id && '- Subscripción Actual'
+                }</p>
+                <p className="title_descrip_list">Costo: S/ {subscription.cost}</p>
+                <p className="title_descrip_list">Comisíón: {subscription.commission}%</p>
+              </div>
+              {
+                subscription.id !== 1 &&  
                 <ButtonsOptions>
                   <Link to={`detalle/${subscription.id}`}>Detalle</Link>
-                  <Link to={`actualizar/${subscription.id}`}>Actualizar</Link>
+                  <Link to={`contratar/${subscription.id}`}>Contratar</Link>
                 </ButtonsOptions>
-              </div>
-            ))
-          }
-        </div>
+              }
+            </div>
+          ))
+        }
+      </div>
     </Main>
   )
 }
 
-export default Subscriptions
+const mapStateToProps = state => ({
+  idSubsCription: state.user.subscription.id
+})
+
+export default connect(mapStateToProps)(Subscriptions)
