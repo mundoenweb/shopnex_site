3ca1d50c948acd5ecda59ad093e29c331a1fb5c9
rommer <rommer@tuemprende.com>
2022-07-24 14:40:05 -0500

    se modifica la lista de noticias para los clientes

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -2,15 +2,10 @@ import ButtonsOptions from "../../components/common/ButtonsOptions"
 import useFetchGet from "../../hooks/useFetchGet"
 import { Link } from 'react-router-dom'
 import Main from "../../components/templetes/Main/Main"
-// import { handleDeleteTask } from "./utils/handleDeleteTask"
 
 const News = () => {
 
   const [News] = useFetchGet('/news')
-
-  // const deleteTask = (id, evt) => {
-  //   handleDeleteTask(evt, id, News, setData)
-  // }
 
   return (
     <Main title='Lista Noticias'>
@@ -20,29 +15,24 @@ const News = () => {
       }
       <div className="box_content">
         {
-          News.map(n => {
-            if (n.id === 1) return <></>
-            return (
-              <div key={n.id} className="item_list_general">
-                <div>
-                  <h2 className="title_item_list">
-                    {n.name}
-                  </h2>
-                    <p className="title_item_list box_descrip_list_news">
-                      {n.description.split('\n')[0]}
-                    </p>
-                </div>
-                <ButtonsOptions>
-                  <Link to={`detalle/${n.id}`}>
-                    Detalle
-                  </Link>
-                  <Link to={`actualizar/${n.id}`}>
-                    Actualizar
-                  </Link>
-                </ButtonsOptions>
+          News.map(n => (
+            <div key={n.id} className="item_list_general">
+              <div>
+                <h2 className="title_item_list">
+                  {n.name}
+                </h2>
+                <p className="title_item_list box_descrip_list_news">
+                  {n.description.split('\n')[0]}
+                </p>
               </div>
-            )
-          })}
+              <ButtonsOptions>
+                <Link to={`detalle/${n.id}`}>
+                  Detalle
+                </Link>
+              </ButtonsOptions>
+            </div>
+          ))
+        }
       </div>
     </Main>
   )
